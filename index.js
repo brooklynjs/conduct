6be2fdbc1784f1765ab6e9c2d912d285cf9d1370
Will Duffy <willmanduffy@gmail.com>
2016-05-20 17:36:19 -0400

    Modify mentions for BrooklynJS

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(require('cors')())
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: false }))
 
-const mentions = ' <@visnup> <@billyroh> <@nexxy> <@kellyk> <@thealphanerd>'
+const mentions = ' <@willman> <@jasmine> <@jenn> <@kosamari> <@brianloveswords>'
 app.use(function (req, res) {
   let body = req.body.Body || req.body.body || req.body.description
 
